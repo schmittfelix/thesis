e217345b9e046e1875622a131fea27d65ecbb725
Felix Schmitt <72166896+schmittfelix@users.noreply.github.com>
2023-02-22 02:49:37 +0100

    first test to get warmed up with networkx/matplotlib

--- tests/main.py
+++ tests/main.py
@@ -1,14 +1,27 @@
-# Create graph
-from networkx import DiGraph
+#%%
+import networkx as nx
+from matplotlib import pyplot as plt
+#%%
+#first drawing test
+G = nx.DiGraph(name="test")
 
-G = DiGraph()
+G.add_node(0, name="root")
+G.add_node(1, name="middle")
+G.add_node(2, name="end")
+G.add_edge(0, 1, weight=5)
+G.add_edge(1, 2, weight=10)
+G.add_edge(2, 0, weight=100)
 
-for v in [1, 2, 3, 4, 5]:
-       G.add_edge("Source", v, cost=10)
-       G.add_edge(v, "Sink", cost=10)
-G.add_edge(1, 2, cost=10)
-G.add_edge(2, 3, cost=10)
-G.add_edge(3, 4, cost=15)
-G.add_edge(4, 5, cost=10)
-
-print(len(G))
+pos = nx.spring_layout(G)
+edge_labels = dict([((n1, n2), f'{G.edges[n1, n2]["weight"]}')
+                     for n1, n2 in G.edges])
+fig, ax = plt.subplots()
+ax.set_title("This is the result of my first test:")
+nx.draw_networkx(G, pos)
+nx.draw_networkx_edge_labels(
+       G, pos, 
+       edge_labels=edge_labels
+)
+plt.box(False)
+plt.show()
+# %%
