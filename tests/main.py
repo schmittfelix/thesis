e49f54afe38c0c7a7eb2733c22228158064aacd7
Felix Schmitt <72166896+schmittfelix@users.noreply.github.com>
2023-03-01 03:00:06 +0100

    real würzburg road network and cspy-arm64
    arm64 arch for cspy to satisfy vrpy dependency on Apple Silicon

--- tests/main.py
+++ tests/main.py
@@ -1,27 +1,16 @@
 #%%
 import networkx as nx
 from matplotlib import pyplot as plt
-#%%
-#first drawing test
-G = nx.DiGraph(name="test")
+import osmnx as ox
 
-G.add_node(0, name="root")
-G.add_node(1, name="middle")
-G.add_node(2, name="end")
-G.add_edge(0, 1, weight=5)
-G.add_edge(1, 2, weight=10)
-G.add_edge(2, 0, weight=100)
+ox.settings.use_cache=True
+# %%
+G = ox.graph_from_place(", Würzburg, Unterfranken, Bayern, Deutschland", network_type="all")
 
-pos = nx.spring_layout(G)
-edge_labels = dict([((n1, n2), f'{G.edges[n1, n2]["weight"]}')
-                     for n1, n2 in G.edges])
-fig, ax = plt.subplots()
-ax.set_title("This is the result of my first test:")
-nx.draw_networkx(G, pos)
-nx.draw_networkx_edge_labels(
-       G, pos, 
-       edge_labels=edge_labels
-)
-plt.box(False)
-plt.show()
+G = ox.add_edge_speeds(G)
+G = ox.add_edge_travel_times(G)
+ox.plot_graph(G)
+
+
+
 # %%
