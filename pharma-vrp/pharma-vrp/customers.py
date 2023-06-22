415ef2e8ba9bd08b3cf7a816afa30fcd7f8f8b63
Felix Schmitt <72166896+schmittfelix@users.noreply.github.com>
2023-06-22 17:24:08 +0200

    first part of customer generation

--- pharma-vrp/pharma-vrp/customers.py
+++ pharma-vrp/pharma-vrp/customers.py
@@ -4,3 +4,13 @@
 inhabitants = 84,3e6
 unit_volume = 852e6
 total_cost = 51,21e9
+
+
+def get_pop_size (area):
+    """Get population size for a given area.
+    
+    This method uses the GENESIS-Online database for population statistics."""
+
+        pop_size_url = "https://www-genesis.destatis.de/genesisWS/rest/2020/data/result?name="
+
+    
