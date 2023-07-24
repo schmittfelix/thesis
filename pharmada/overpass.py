295ae676cc4954e7c5755637d27a9c2db5056d6d
Felix Schmitt <72166896+schmittfelix@users.noreply.github.com>
2023-07-25 00:39:04 +0200

    mid-shift update

--- pharmada/overpass.py
+++ pharmada/overpass.py
@@ -25,7 +25,7 @@ def resolve_reg_key(regional_key):
     response = query_overpass(query)
 
     # If the response is empty, add trailing zeros to the regional key to form a full 12-digit key
-    if not response:
+    if not response['elements']:
         regional_key = f"{regional_key:0<12}"
         query = f"""
             [out:json];
@@ -52,32 +52,34 @@ def resolve_reg_key(regional_key):
         raise ValueError("No relation found for regional key.")
     
     # If the response contains no regional key, raise an error
-    if 'de:regionalschluessel' not in result['tags']:
+    if not result['tags']['de:regionalschluessel']:
         raise ValueError("No regional key found for relation.")
     
     # Check if the regional keys from the request and the result match
-    if regional_key != response['tags']['de:regionalschluessel']:
+    original_key = f'{regional_key:0<12}'
+    result_key = result['tags']['de:regionalschluessel']
+    if original_key != result_key:
         raise ValueError("Regional keys from request and result do not match.")
     
     # If the response contains no name or id, raise an error
-    if 'name' or 'id' not in result['tags']:
+    if not result['id'] or not result['tags']['name']:
         raise ValueError("No name or id found for relation.")
     
     # Result is valid, return it
     data = {
-        'osm_id':       response['id'],
-        'regional_key': response['tags']['de:regionalschluessel'],
+        'osm_id':       result['id'],
+        'regional_key': result['tags']['de:regionalschluessel'],
     }
 
     # Add name with prefix if it exists
-    if 'name:prefix' in response['tags']:
+    if 'name:prefix' in result['tags']:
         n = []
-        n.append(response['tags']['name:prefix'])
+        n.append(result['tags']['name:prefix'])
         n.append(' ')
-        n.append(data['name'])
+        n.append(result['tags']['name'])
         data['name'] = ''.join(n)
     else:
-        data['name'] = response['tags']['name']
+        data['name'] = result['tags']['name']
 
     return data
 
@@ -105,37 +107,40 @@ def get_area_geometry(regional_key):
     if len(response['elements']) > 1:
         raise ValueError("Multiple results for OSM ID.")
     
+    # Existance and singularity of the result is guaranteed at this point
+    result = response['elements'][0]
+    
     # If the result is not a relation, raise an error
-    if response['elements'][0]['type'] != 'relation':
+    if result['type'] != 'relation':
         raise ValueError("No relation found for OSM ID.")
     
-    # If the response contains no geometry, raise an error
-    if 'geometry' not in response['elements'][0]:
-        raise ValueError("No geometry found for relation.")
-    
     # If the response contains no name or id, raise an error
-    if 'name' or 'id' not in response['elements'][0]['tags']:
+    if 'id' not in result or 'name' not in result['tags']:
         raise ValueError("No name or id found for relation.")
 
     # If the IDs from the request and the result do not match, raise an error
-    if osm_id != response['elements'][0]['id']:
+    if osm_id != result['id']:
         raise ValueError("IDs from request and result do not match.")
     
-    # -> we can safely access the object
+    # If the response contains no geometry, raise an error
+    if not result['members']:
+        raise ValueError("No geometry found for relation.")
     
-    # Convert the response to GeoJSON
-    result = o2g.json2geojson(response)['features'][0]['geometry']
+    # -> Result is valid, continue with geometry extraction
 
-    # Create a GeoDataFrame from the GeoJSON
-    area_geom = gpd.read_file(json.dumps(result), driver='GeoJSON')
+    # Extract the geometry from the response
+    geometry = o2g.json2shapes(response)[0]['shape']
+    
+    # Convert the geometry to a GeoDataFrame
+    area_geom = gpd.GeoDataFrame(geometry=[geometry], crs='EPSG:4326')
 
-    # Add the regional key and name to the GeoDataFrame
+    # Add regional key and name to the GeoDataFrame
     area_geom['regional_key'] = regional_key
     area_geom['name'] = name
 
     # If the area is a multipolygon, convert it to a polygon
     if area_geom.geom_type[0] == 'MultiPolygon':
-        area_geom = area_geom.explode()
+        area_geom = area_geom.explode(index_parts=False)
         area_geom = area_geom.reset_index(drop=True)
 
     return area_geom
--- pharmada/pharmacies.py
+++ pharmada/pharmacies.py
@@ -4,6 +4,7 @@ Area geometry is extracted from OSM.
 Pharmacy data is retreived from GMaps.
 """
 
+from sklearn import base
 import pharmada.overpass as op
 import time
 import ftfy
@@ -17,11 +18,8 @@ def pharmacies_in_area(regional_key, gmaps_key):
     # get area geometry
     area_geom = op.get_area_geometry(regional_key)
 
-    # calculate area radius
-    area_radius = calc_area_radius(area_geom)
-
     # get pharmacies
-    found_pharmacies = fetch_pharmacies(area_geom, area_radius, gmaps_key)
+    found_pharmacies = fetch_pharmacies(area_geom, gmaps_key)
 
     # filter pharmacies
     pharmacies = filter_pharmacies(found_pharmacies, area_geom)
@@ -35,33 +33,84 @@ def calc_area_radius(area_geom):
     
     area_radius = area_geom.minimum_bounding_radius()
 
-    return area_radius[0]
+    return area_radius[0].round(0)
 
-def fetch_pharmacies(area_geom, area_radius, gmaps_key):
+def fetch_pharmacies(area_geom, gmaps_key):
     """Get pharmacies within area from GMaps."""
 
-    area_geom = area_geom.to_crs(area_geom.estimate_utm_crs())
+    # convert area geometry to UTM
+    crs = area_geom.estimate_utm_crs()
+    area_geom = area_geom.to_crs(crs)
 
+    # get area centroid and convert back to WGS84 for LatLng coordinates
     centroid = area_geom.centroid
-    centroid = centroid.to_crs('epsg:4326')
+    centroid = centroid.to_crs('EPSG:4326')
+
+    # calculate search radius
+    search_radius = calc_area_radius(area_geom)
+
+    #Subfunction to query the GMaps Places API
+    def query_gmaps(query_params):
+        """Query the GMaps Places API ."""
+        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
     
-    find_pharmacies = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={centroid.y[0]},{centroid.x[0]}&radius={area_radius}&type=pharmacy&language=de&key={gmaps_key}'
-    result = req.get(find_pharmacies).json()
+        # set default parameter
+        payload = {'key': gmaps_key}
+
+        # add query parameters
+        for key, value in query_params.items():
+            payload[key] = value
+
+        # query the API
+        response = req.get(url, params=payload)
+
+        # check if the request was successful        
+        response.raise_for_status()
+        
+        # If the response is empty, raise an error
+        if not response['results']:
+            raise ValueError("No results found.")
+            
+        # convert response to JSON
+        result = response.json()
+        return result
+
+    # get pharmacies from GMaps
+    params = {
+        'location': f'{centroid.y[0]},{centroid.x[0]}',
+        'radius': search_radius,
+        'type': 'pharmacy',
+        'language': 'de'
+    }
+    result = query_gmaps(params)
+
 
     found_pharmacies = result['results']
 
-    # Max 20 results are returned per request, so we need to use the next_page_token to get more results if available
-    if 'next_page_token' in result:
-        
-        while True:
-            find_more_pharmacies = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={result["next_page_token"]}&key={gmaps_key}'
-            
-            time.sleep(5)
-            result = req.get(find_more_pharmacies).json()
-            found_pharmacies.extend(result['results'])
+    # API returns max. 20 results per request, next_page_token is used for pagination.
+    # Max. of 3 pages (60 results) are returned.
+    def get_next_page(next_page_token):
+        """Get the next page of results from GMaps."""
 
-            if 'next_page_token' not in result:
-                break
+        # short pause due to API limitations
+        time.sleep(5)
+
+        # resend request with next_page_token exclusively
+        params.clear()
+        params['pagetoken'] = next_page_token
+        result = query_gmaps(params)
+
+        return result
+    
+    # check if there are more results
+    for i in range(2):
+        if 'next_page_token' in result:
+            # get next page
+            result = get_next_page(result['next_page_token'])
+
+            found_pharmacies.extend(result['results'])
+        else:
+            break
 
     return found_pharmacies
 
